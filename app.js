e6b260f5625ca2c2930661a163bf24edb03a0a35
j-marcos-b <correodemarcosb@gmail.com>
2024-04-10 21:39:55 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const taskInput = document.querySelector('#taskInput');
+const addTaskBtn = document.querySelector('#addTaskBtn');
+const taskList = document.querySelector('#taskList');
+
+addTaskBtn.addEventListener('click', () => {
+  const taskTest = taskInput.value.trim();
+
+  if (taskTest !== '') {
+    const li = document.createElement('li');
+    li.textContent = taskTest;
+    li.classList = 'item'
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Eliminar';
+    deleteBtn.classList = 'delete-btn';
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    
+
+    taskInput.value = '';
+
+    deleteBtn.addEventListener('click', () => {
+      li.remove();
+    })
+    
+  } else {
+    alert('Ingrese una tarea');
+  }
+})
